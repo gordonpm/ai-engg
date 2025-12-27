2eea55ad69c3d97af4eb1e6edc20a5764aabdb41
gordonpm <gordonm0815@gmail.com>
2025-12-27 14:12:10 -0800

    added training loop for convergence

--- PyTorch/training_neuron.py
+++ PyTorch/training_neuron.py
@@ -1,4 +1,3 @@
-import torch
 from torch import nn
 from torch import float32
 from torch import tensor
@@ -12,17 +11,25 @@ y2 = tensor([[100.0]], dtype=float32)
 
 model = nn.Linear(1, 1)
 loss_fn = nn.MSELoss()
-optimizer = optim.SGD(model.parameters(), lr=0.001)
+optimizer = optim.SGD(model.parameters(), lr=0.0001)
 
-# Training pass
-print("weight: ", model.weight)
-optimizer.zero_grad()
-outputs = model(X1)
-loss = loss_fn(outputs, y1)
-loss.backward()
-optimizer.step()
-print("weight: ", model.weight)
-print("loss: ", loss)
+# Training
+for i in range(0, 100000):
+    optimizer.zero_grad()
+    outputs = model(X1)
+    loss = loss_fn(outputs, y1)
+    loss.backward()
+    optimizer.step()
+
+    optimizer.zero_grad()
+    outputs = model(X2)
+    loss = loss_fn(outputs, y2)
+    loss.backward()
+    optimizer.step()
+
+    if i % 1000 == 0:
+        print("weight: ", model.weight)
+        print("bias: ", model.bias)
 
 y1_pred = model(X1)
 print("y1_pred: ", y1_pred)
